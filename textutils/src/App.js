1adf2e14d349f9f55be8fb9ecc998dd7e886e56d
sahill-1 <sv260397@gmail.com>
2023-02-20 11:52:53 +0530

    completed-12

--- textutils/src/App.js
+++ textutils/src/App.js
@@ -1,14 +1,26 @@
+import React, { useState } from 'react';
 import './App.css';
-import About from './components/About';
+// import About from './components/About';
 import Navbar from './components/Navbar';
-// import TextForm from './components/TextForm';
+import TextForm from './components/TextForm';
 function App() {
+  const [mode, setMode] = useState("light");
+  const toggleMode = () => {
+    if(mode==="light"){
+      setMode("dark")
+      document.body.style.backgroundColor = "#042743";
+    }
+    else{
+      setMode("light")
+      document.body.style.backgroundColor = "white";
+    }
+  }
   return (
     <>
-      <Navbar title="TextUtils2"/>
+      <Navbar title="TextUtils2" mode={mode} toggleMode={toggleMode}/>
       <div className="container my-3">
-        {/* <TextForm heading="Enter Your Text To Analyse"/> */}
-        <About/>
+        <TextForm heading="Enter Your Text To Analyse" mode={mode}/>
+        {/* <About/> */}
       </div>
     </>
   );
--- textutils/src/components/TextForm.js
+++ textutils/src/components/TextForm.js
@@ -19,20 +19,20 @@ export default function TextForm(props) {
     const [text, setText] = useState("Enter your text")
     return (
         <>
-        <div className="container">
+        <div className="container" style={{color: props.mode==="dark" ? "white" : "#042743"}}>
             <h1>{props.heading}</h1>
             <div className="mb-3">
-                <textarea className="form-control" value={text} onChange={handleOnChange} id="exampleFormControlTextarea1" rows="8"></textarea>
-                <button className="btn btn-primary mx-2" onClick={handleClickUpperCase}>Convert to UpperCase</button>
-                <button className="btn btn-primary mx-2" onClick={handleClickLowerCase}>Convert to LowerCase</button>
+                <textarea className="form-control" value={text} style={{backgroundColor: props.mode==="dark" ? "grey" : "white", color: props.mode==="dark" ? "white" : "#042743"}} onChange={handleOnChange} id="exampleFormControlTextarea1" rows="8"></textarea>
+                <button className="btn btn-primary mx-2 my-3" onClick={handleClickUpperCase}>Convert to UpperCase</button>
+                <button className="btn btn-primary mx-2 my-3" onClick={handleClickLowerCase}>Convert to LowerCase</button>
             </div>
         </div>
-        <div className="container my-3">
+        <div className="container my-3" style={{color: props.mode==="dark" ? "white" : "#042743"}}>
             <h2>Your Text Summary</h2>
             <p>{text.split(" ").length} words & {text.length} characters</p>
             <p>{0.008 * text.split(" ").length } minutes read</p>
             <h2>Preview</h2>
-            <p>{text}</p>
+            <p>{text.length > 0 ? text : "Type in box to preview it here."}</p>
         </div>
             
         </>
